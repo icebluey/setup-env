935a1d1fe441a09cf638098ad33b0c95f7cfd093
Kenneth Davidson <xylene2016@protonmail.com>
2022-01-31 07:50:21 -0500

    Update setup-env.sh

--- setup-env.sh
+++ setup-env.sh
@@ -11,6 +11,7 @@ _install_ssl_111() {
     cd "${_tmp_dir}"
     install -m 0755 -d openssl
     cd openssl
+    yum install -y zlib glibc
     _ssl_111_ver='1.1.1m'
     wget -c -t 9 -T 9 "https://github.com/icebluey/openssl/releases/download/${_ssl_111_ver}/openssl1.1-${_ssl_111_ver}-1.el7.x86_64.rpm"
     wget -c -t 9 -T 9 "https://github.com/icebluey/openssl/releases/download/${_ssl_111_ver}/openssl1.1-devel-${_ssl_111_ver}-1.el7.x86_64.rpm"
