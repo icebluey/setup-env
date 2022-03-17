7c9c2d49fbaeff295c02a5fd0d44da5e761ba984
Kenneth Davidson <xylene2016@protonmail.com>
2022-03-16 23:25:04 -0400

    Update setup-env.sh

--- setup-env.sh
+++ setup-env.sh
@@ -140,7 +140,7 @@ yum install -y perl perl-devel perl-libs perl-Env perl-ExtUtils-Embed \
   perl-ExtUtils-ParseXS perl-Git perl-JSON perl-SGMLSpm perl-libwww-perl perl-podlators
 yum update -y
 
-_kernel_ver='5.15.28-20220311'
+_kernel_ver='5.15.29-20220316'
 yum install -y "https://github.com/icebluey/kernel/releases/download/v$(echo ${_kernel_ver} | cut -d- -f1)/kernel-headers-${_kernel_ver}.el7.x86_64.rpm"
 yum install -y "https://github.com/icebluey/kernel/releases/download/v$(echo ${_kernel_ver} | cut -d- -f1)/kernel-devel-${_kernel_ver}.el7.x86_64.rpm"
 yum install -y "https://github.com/icebluey/kernel/releases/download/v$(echo ${_kernel_ver} | cut -d- -f1)/kernel-tools-libs-${_kernel_ver}.el7.x86_64.rpm" \
