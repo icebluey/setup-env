cf01e675c3b22721dbab3d84daf1c8d2cc72f093
Kenneth Davidson <xylene2016@protonmail.com>
2022-03-19 09:12:15 -0400

    Update setup-env.sh

--- setup-env.sh
+++ setup-env.sh
@@ -137,8 +137,8 @@ _install_gpg2() {
     yum install -y readline-devel readline
     yum install -y bzip2-devel bzip2-libs bzip2
     yum install -y gnupg2 gpgme-devel gpgme libassuan-devel libassuan libgcrypt-devel libgcrypt libgpg-error-devel libgpg-error libksba-devel libksba pth-devel pth
-    tar -xf .pre/libedit-*.tar.xz -C /
-    tar -xf .pre/sqlite-*.tar.xz -C /
+    tar -xf .pre/libedit-*.x86_64.tar.xz -C /
+    tar -xf .pre/sqlite-*.x86_64.tar.xz -C /
     bash .del-old.so.sh ; bash .install_all.sh
     sleep 2
     /sbin/ldconfig >/dev/null 2>&1
