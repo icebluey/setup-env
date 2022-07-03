6d140dc9a3bfbbedb5792f3cf5bff761b737690f
Kenneth Davidson <xylene2016@gmail.com>
2022-07-02 23:39:12 -0400

    Add files via upload

---
+++ runme.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+export PATH=$PATH:/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
+TZ='UTC'; export TZ
+
+umask 022
+
+set -e
+
+if ! grep -q '^alias ll=' ~/.bashrc; then echo "alias ll='/bin/ls --color -lah'" >> ~/.bashrc; . ~/.bashrc; fi
+
+#echo "proxy=http://192.168.10.1:1081" >> /etc/yum.conf
+#export http_proxy="http://192.168.10.1:1081"
+#export https_proxy="http://192.168.10.1:1081"
+
+_setup_env() {
+    set -e
+    _tmp_dir="$(mktemp -d)"
+    cd "${_tmp_dir}"
+    wget -c -t 9 -T 9 "https://raw.githubusercontent.com/icebluey/setup-env/master/setup-env.sh"
+    bash setup-env.sh
+    cd /tmp
+    rm -fr "${_tmp_dir}"
+    /sbin/ldconfig
+}
+
+yum install -y deltarpm
+yum install -y bash wget ca-certificates
+yum update -y deltarpm bash wget ca-certificates
+ln -svf bash /bin/sh
+
+_setup_env
+
+
