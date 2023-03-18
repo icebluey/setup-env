faf570f96bd91c67c044375d418c6100df3e6c58
Kenneth Davidson <xylene2016@protonmail.com>
2023-03-18 12:05:42 +0800

    Update setup-env.sh

--- setup-env.sh
+++ setup-env.sh
@@ -38,7 +38,7 @@ _install_openssh() {
     cd "${_tmp_dir}"
     install -m 0755 -d openssh
     cd openssh
-    _ssh_ver='9.2p1-20230211'
+    _ssh_ver='9.3p1-20230318'
     wget -c -t 9 -T 9 "https://github.com/icebluey/openssh/releases/download/${_ssh_ver}/openssh-${_ssh_ver}.el7.x86_64.rpm"
     wget -c -t 9 -T 9 "https://github.com/icebluey/openssh/releases/download/${_ssh_ver}/openssh-clients-${_ssh_ver}.el7.x86_64.rpm"
     wget -c -t 9 -T 9 "https://github.com/icebluey/openssh/releases/download/${_ssh_ver}/openssh-server-${_ssh_ver}.el7.x86_64.rpm"
